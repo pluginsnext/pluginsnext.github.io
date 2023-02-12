1b98898c7e6cf0a623511abf2a7974c8aaa4d623
utsav <mail.utsav02@gmail.com>
2023-02-12 18:14:47 +0530

    bootstrap added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,16 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>PluginsNext</title>
         <link rel="shortcut icon" href="/static/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        />
+
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
