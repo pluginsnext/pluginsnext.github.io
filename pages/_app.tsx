1f16f95adac31315d9866fa0f03074cdee7a7011
utsav <mail.utsav02@gmail.com>
2023-02-12 18:40:04 +0530

    bootstrap latest version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,13 +9,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>PluginsNext</title>
         <link rel="shortcut icon" href="/static/favicon.ico" />
         <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+          crossOrigin="anonymous"
         />
 
         <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+          integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
           crossOrigin="anonymous"
         ></script>
       </Head>
